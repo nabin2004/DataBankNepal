17894521f1c75f2d1cce2fc33dc95f327e265155
nabin2004 <olinabin2004@gmail.com>
2024-02-21 15:24:06 +0545

    Almost completed

--- src/components/DataCard.jsx
+++ src/components/DataCard.jsx
@@ -6,18 +6,18 @@ const DataCard = () => {
         <div className='flex flex-col gap-5 overflow-auto scroll-smooth' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
 
           <div>
-            <h2 className='font-bold'>About Data</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum harum accusantium sed accusamus non distinctio, debitis numquam, vel dolorum illo dolorem itaque odio aut laudantium. Eius dignissimos ut eos doloremque enim minima qui nesciunt! Autem inventore repudiandae odit nesciunt sapiente!</p>
+            <h2 className='font-bold '>About Data</h2>
+            <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum harum accusantium sed accusamus non distinctio, debitis numquam, vel dolorum illo dolorem itaque odio aut laudantium. Eius dignissimos ut eos doloremque enim minima qui nesciunt! Autem inventore repudiandae odit nesciunt sapiente!</p>
           </div>
 
           <div>
             <h2 className='font-bold'>Data Info</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, distinctio nostrum adipisci incidunt ipsam quo. Repellendus provident deserunt ab perferendis modi expedita ullam reprehenderit neque enim? Suscipit, dolorum, iusto nostrum, pariatur fugit accusamus aliquam fugiat culpa mollitia debitis soluta unde perspiciatis error omnis quae deserunt natus dolores. Quae, illo sequi!</p>
+            <p className='text-justify'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, distinctio nostrum adipisci incidunt ipsam quo. Repellendus provident deserunt ab perferendis modi expedita ullam reprehenderit neque enim? Suscipit, dolorum, iusto nostrum, pariatur fugit accusamus aliquam fugiat culpa mollitia debitis soluta unde perspiciatis error omnis quae deserunt natus dolores. Quae, illo sequi!</p>
           </div>
 
           <div>
             <h2 className='font-bold'>Research Methodologies</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam praesentium ex modi ipsa assumenda sunt eligendi dignissimos atque, iure obcaecati cumque placeat mollitia in sapiente esse nostrum officia id vitae.</p>
+            <p className='text-justify'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam praesentium ex modi ipsa assumenda sunt eligendi dignissimos atque, iure obcaecati cumque placeat mollitia in sapiente esse nostrum officia id vitae.</p>
           </div>
         </div>
       </div>
--- src/components/code.jsx
+++ src/components/code.jsx
@@ -2,8 +2,8 @@ import InsightsBox from "./insightsBox";
 
 function Code(){
     return(
-        <div>
-            <InsightsBox text='Healtcare DataAnalysis'/>
+        <div className="w-70%">
+           <a href='https://github.com/nabin2004/US-Accident-EDA/blob/main/1_EDA_on_US-ACCIDENTS%20.ipynb'>  <InsightsBox text='Healtcare DataAnalysis'/> </a>
         </div>
     )
 }
